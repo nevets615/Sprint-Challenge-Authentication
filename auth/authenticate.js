f5d669ecef16ef8dfc2b2425f712b517c5ba71a0
nevets615 <surj316@yahoo.com>
2019-05-10 12:52:47 -0400

    working through router

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,17 +1,27 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const jwtKey =
   process.env.JWT_SECRET ||
-  'add a .env file to root of project with the JWT_SECRET variable';
+  "add a .env file to root of project with the JWT_SECRET variable";
 
 // quickly see what this file exports
 module.exports = {
   authenticate,
+  generateToken
 };
-
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const secret = "keep it secret";
+  const options = {
+    expiresIn: "1h"
+  };
+  return jwt.sign(payload, secret, options);
+}
 // implementation details
 function authenticate(req, res, next) {
-  const token = req.get('Authorization');
+  const token = req.get("Authorization");
 
   if (token) {
     jwt.verify(token, jwtKey, (err, decoded) => {
@@ -23,7 +33,7 @@ function authenticate(req, res, next) {
     });
   } else {
     return res.status(401).json({
-      error: 'No token provided, must be set on the Authorization Header',
+      error: "No token provided, must be set on the Authorization Header"
     });
   }
 }
--- config/routes.js
+++ config/routes.js
@@ -1,32 +1,68 @@
-const axios = require('axios');
-
-const { authenticate } = require('../auth/authenticate');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const { authenticate, generateToken } = require("../auth/authenticate");
+const knex = require("knex");
+const knexConfig = require("../knexfile");
+const db = knex(knexConfig.development);
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
-  // implement user registration
+  const creds = req.body;
+  creds.password = bcrypt.hashSync(creds.password, 10);
+  db(users)
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+      db("users")
+        .where("id", id)
+        .first()
+        .then(users => {
+          const token = generateToken(users);
+          res.status(201).json({ id: users.id, token });
+        })
+        .catch(err => {
+          res.status(500).send(err);
+        });
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
 }
 
 function login(req, res) {
-  // implement user login
+  const creds = req.body;
+  db("users")
+    .where("usersname", creds.username)
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(users);
+        res.json({ message: user.username, token });
+      } else {
+        res.status(401).send("not valad");
+      }
+    })
+    .catch(err => {
+      res.status(500).send(err);
+    });
 }
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
